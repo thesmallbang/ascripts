3a815ba6657c1e88a0f7f1d909941669d9cf9800
Daniel Dorantes <daniel.dorantes@oesglobal.com>
2019-04-14 14:24:31 -0500

    pyre update

--- helper.lua
+++ helper.lua
@@ -67,7 +67,7 @@ function OnGMCP(text)
         res, gmcparg = CallPlugin("3e7dedbe37e44942dd46d264", "gmcpval", "char")
         luastmt = "gmcpdata = " .. gmcparg
         assert(loadstring(luastmt or ""))()
-        Core.Status.Room = gmcpval("info.name")
+        Core.Status.Room = gmcpval("name")
         Core.Status.RoomId = tonumber(gmcpval("num"))
         Core.Log("room " .. Core.Status.Room, Core.LogLevel.DEBUG)
     end

9292c61e63c262bacf69a68e697f1dc855a3cd76
Daniel Dorantes <daniel.dorantes@oesglobal.com>
2019-05-06 00:39:56 -0500

    version

--- pyrehelper.lua
+++ pyrehelper.lua
@@ -6,7 +6,7 @@ Pyre.Log('helper.lua loaded', Pyre.LogLevel.DEBUG)
 
 local Helper = {}
 
-local Version = '1.2.15'
+local Version = '1.2.16'
 local Features = {
     {Name = 'skills', Feature = {}, Encapsulated = true},
     {Name = 'scanner', Feature = {}, Encapsulated = true}

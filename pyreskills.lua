2ff7d803c0c18e68f9ce5b119810ceb97f4cf121
Daniel Dorantes <daniel.dorantes@oesglobal.com>
2019-05-07 19:20:18 -0500

    fixed extra quaffs

--- pyreskills.lua
+++ pyreskills.lua
@@ -2073,7 +2073,14 @@ function ResetAttackQueue()
     Pyre.Log('Resetting attack queue', Pyre.LogLevel.VERBOSE)
     SkillFeature.SkillFail = nil
     SkillFeature.LastSkill = nil
-    SkillFeature.AttackQueue = {}
+    SkillFeature.AttackQueue =
+        Pyre.Except(
+        SkillFeature.AttackQueue,
+        function(v)
+            return ((v.Skill.SkillType == Pyre.SkillType.QuaffHeal) or (v.Skill.SkillType == Pyre.SkillType.QuaffMana) or
+                (v.Skill.SkillType == Pyre.SkillType.QuaffMove))
+        end
+    )
 end
 
 function OnStateChange(stateObject)

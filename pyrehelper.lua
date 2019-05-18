56964d069741cd24eaf53b4e529a72259c2b4e03
Daniel Dorantes <daniel.dorantes@oesglobal.com>
2019-05-18 05:40:23 -0500

    still deciding on how i want it to work

--- pyrehelper.lua
+++ pyrehelper.lua
@@ -82,11 +82,6 @@ function PH.Install(remoteVersionData, featuresOnDisk)
 
     Core.Log(PH.Config.Versions.Release.Version .. ' - ' .. PH.Config.Versions.Release.Description)
 
-    if (PH.Config.Versions.Release.Version ~= PH.Config.LatestVersions.Release.Version) then
-        Core.Log('Update Available. ' .. PH.Config.LatestVersions.Release.Version)
-        Core.Log(PH.Config.LatestVersions.Release.Description)
-    end
-
     PH.Start()
 end
 
